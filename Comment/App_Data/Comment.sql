adcca5eb5ca3eaca629c1a7b00c9484a67d29f51
Yelzhas Sagidulla <yelzhass@gmail.com>
2017-07-23 12:27:12 +0400

    parent id

--- Comment/App_Data/Comment.sql
+++ Comment/App_Data/Comment.sql
@@ -5,6 +5,7 @@
     [CreatedAt] TIMESTAMP   NOT NULL,
     [ArticleId] INT          NOT NULL,
     [StatusId]  INT          DEFAULT ((1)) NULL,
+    [ParentId]  INT          NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     FOREIGN KEY ([ArticleId]) REFERENCES [dbo].[Article] ([Id]),
     FOREIGN KEY ([StatusId]) REFERENCES [dbo].[Status] ([Id])
--- Comment/App_Data/GetCommentsProcedure.sql
+++ Comment/App_Data/GetCommentsProcedure.sql
@@ -1,19 +1,17 @@
 ﻿CREATE PROCEDURE Get_Comments
-	@StartDate date,
-	@EndDate date
+	@StartDate datetime,
+	@EndDate datetime
 AS
 	SELECT
 		cmnt.Id,
-		cmnt.Content,
-		cmnt.Username,
-		cmnt.CreatedAt,
-		artcl.Id as ArticleId,
-		artcl.Title as ArticleTitle,
-		artcl.Description as ArticleDescription,
-		st.Id as StatusId,
+		Content,
+		Username,
+		CreatedAt,
+		ArticleId,
+		StatusId,
+		cmnt.ParentId,
 		st.Title as StatusTitle
 	FROM Comment cmnt
-	JOIN Article artcl ON artcl.Id = cmnt.ArticleId
-	JOIN Status st ON st.Id = cmnt.StatusId
-	WHERE cmnt.CreatedAt BETWEEN @StartDate AND @EndDate
+	JOIN Status st ON cmnt.StatusId = st.Id
+	WHERE StatusId = 2 AND CreatedAt BETWEEN @StartDate AND @EndDate;
 RETURN 0
